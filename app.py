92a5ff8912d66cce9a18b21aadf0387c6aaea513
Dave Leigh <thejayhawk@gmail.com>
2015-06-03 03:04:58 +0000

    adjusting postgres config

--- app.py
+++ app.py
@@ -3,7 +3,6 @@ import os, urlparse
 from flask import Flask, render_template, request, redirect, url_for
 
 from peewee import *
-from flask_peewee.db import Database
 
 urlparse.uses_netloc.append('postgres')
 db_url = urlparse.urlparse(os.environ['DATABASE_URL'])
@@ -18,8 +17,7 @@ DATABASE = {
 }
 
 app = Flask(__name__, instance_relative_config=True)
-app.config.from_object(__name__)
-LIST_DB = Database(app)
+LIST_DB = PostgresqlDatabase(DATABASE)
 
 # create a settings.cfg in the base directory with the uncommented line:
 # SECRET_KEY = 'yourGibberishStringHere'
