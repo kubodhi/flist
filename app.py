5af45e6a33a168f739eb0758cea1a85b393b56b2
Dave Leigh <thejayhawk@gmail.com>
2015-06-04 22:06:51 +0000

    cleanup of docstrings, adding before/after request handlers

--- app.py
+++ app.py
@@ -6,21 +6,27 @@ import psycopg2
 from flask import Flask, render_template, request, redirect, url_for
 from peewee import *
 
-DEBUG=True
-
-urlparse.uses_netloc.append('postgres')
-url = urlparse.urlparse(os.environ['DATABASE_URL'])
-DATABASE = {
-    'engine': 'peewee.PostgresqlDatabase',
-    'name': url.path[1:],
-    'user': url.username,
-    'password': url.password,
-    'host': url.hostname,
-    'port': url.port,
-    }
+# Set up DB
+if 'HEROKU' in os.environ:
+    DEBUG=False
+    APP_PORT=5000
+    urlparse.uses_netloc.append('postgres')
+    url = urlparse.urlparse(os.environ['DATABASE_URL'])
+    DATABASE = {
+        'engine': 'peewee.PostgresqlDatabase',
+        'name': url.path[1:],
+        'user': url.username,
+        'password': url.password,
+        'host': url.hostname,
+        'port': url.port,
+        }
+    LIST_DB = PostgresqlDatabase(DATABASE['name'], host=DATABASE['host'], port=DATABASE['port'], user=DATABASE['user'], password=DATABASE['password'])
+else:
+    DEBUG=True
+    APP_PORT=8080
+    LIST_DB = SqliteDatabase('listitem.db')
 
 app = Flask(__name__)
-LIST_DB = PostgresqlDatabase(DATABASE['name'], host=DATABASE['host'], port=DATABASE['port'], user=DATABASE['user'], password=DATABASE['password'])
 
 # create a settings.cfg in the base directory with the uncommented line:
 # SECRET_KEY = 'yourGibberishStringHere'
@@ -35,19 +41,31 @@ class ListItem(Model):
         database = LIST_DB
 
 def initialize():
-    ''' initialize the DB if needed '''
+    """Initialize the DB safely."""
     LIST_DB.connect()
     LIST_DB.create_tables([ListItem], safe=True)
+    LIST_DB.close()
+
+@app.before_request
+def before_request():
+    """Connect to the database before each request."""
+    LIST_DB.connect()
+
+@app.after_request
+def after_request(response):
+    """Close the database connection after each request."""
+    LIST_DB.close()
+    return response
 
 @app.route('/add', methods=['POST'])
 def add_item():
-    ''' add items to the list '''
+    """Add items to the list."""
     ListItem.create(content=request.form['contentadd'])
     return redirect(url_for('view_items'))
 
 @app.route('/view')
 def view_items():
-    ''' view items in the list '''
+    """View items in the list."""
     items = ListItem.select()
     if items:
         return render_template('view.html', items=items)
@@ -56,14 +74,14 @@ def view_items():
 
 @app.route('/delete/<this_id>')
 def delete_item(this_id):
-    ''' delete items from the list '''
+    """Delete items from the list."""
     q = ListItem.delete().where(ListItem.id == this_id)
     q.execute()
     return redirect(url_for('view_items'))
 
 @app.route('/strike/<this_id>')
 def strike(this_id):
-    ''' move items to and from deletion staging area '''
+    """Move items to and from deletion staging area."""
     rec = ListItem.get(ListItem.id == this_id)
     if rec.strike:
       q = ListItem.update(strike=False).where(ListItem.id == this_id)
@@ -75,9 +93,9 @@ def strike(this_id):
 
 @app.route('/')
 def home():
-    ''' application root '''
+    """Application root."""
     return redirect(url_for('view_items'))
 
 if __name__ == '__main__':
     initialize()
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=APP_PORT)
